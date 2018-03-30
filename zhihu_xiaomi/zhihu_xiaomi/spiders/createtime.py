46537048ec10bfa2b6e891dcd83ca69ac7cf8d92
Johnny Liu <8897203@163.com>
2018-03-30 19:26:02 +0800

    zhihu

--- zhihu_xiaomi/zhihu_xiaomi/spiders/createtime.py
+++ zhihu_xiaomi/zhihu_xiaomi/spiders/createtime.py
@@ -13,20 +13,19 @@ class FollowingSpider(Spider):
 
     moclient = MongoClient ('localhost', 27017)
     #moclient = MongoClient ('192.168.7.16', 27017)
-    db = moclient.zhihuliker1
+    db = moclient.zhihu_maoyizhan
     db.collection_names (include_system_collections=False)
-    posts = db.users
+    posts = db.voters
 
-    url = 'https://www.zhihu.com/api/v4/members/{user}/activities?limit=5'
+    url = 'https://www.zhihu.com/api/v4/members/{user}/activities?limit=7'
     url2 = 'https://www.zhihu.com/api/v4/members/{user}/activities?limit=20&after_id={after_id}&desktop=True'
 
     def start_requests(self):
-        for post in self.posts.find(no_cursor_timeout=True):
-           name = post['url_token']
-           print(name)
-           yield Request(self.url.format(user=name),
+        for post in self.posts.find().limit(2):
+            name = post['url_token']
+            yield Request(self.url.format(user=name),
                    self.parse_follows,dont_filter = True)
-        self.posts.close()
+        #self.posts.close()
         #yield Request(self.url3 , self.parse_follows, dont_filter=True)
 
     def parse_follows(self, response):
@@ -34,23 +33,28 @@ class FollowingSpider(Spider):
         signal = True
 
         if 'data' in results.keys():
+
             for result in results.get('data'):
                 type = result.get('verb')
                 id = result.get('actor').get('id')
                 target_id = result.get('target').get('id')
-                target_time = result.get('target').get('created_time')
+                #target_time = result.get('target').get('created_time')
                 created_time = result.get ('created_time')
                 if type == 'ANSWER_VOTE_UP' and target_id == target_id:
 
                     print('I gotcha!!!')
-                    self.db.users.update({'id': id}, {"$set": {"created_time": created_time}})
+                    self.db.voters.update({'id': id}, {"$set": {"created_time": created_time}})
                     signal = False
                     break
 
-                if created_time < target_time:
+                if created_time < 1521834812:
                     print('wo de cuo?')
                     signal = False
                     break
+
+        else:
+            print('匿名用户？')
+            signal = False
 
         if signal:
             if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
